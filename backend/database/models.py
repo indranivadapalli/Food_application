f5498f31081801ba5e97baba27b0a2d5f000a364
shanmukhapriya26 <shanmukhapriya0526@gmail.com>
2026-01-03 11:30:50 +0530

    updated models with new tables

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,76 +1,100 @@
-from sqlmodel import SQLModel,Field
-from typing import List,Optional
+from typing import Optional,List
 from datetime import datetime
+from sqlmodel import SQLModel,Field,Relationship
 
+#user table creation
 class User(SQLModel,table=True):
+    __tablename__="users"
     id:Optional[int]=Field(default=None,primary_key=True)
     name:str
     email:str
     mobile:str
-    address:str
     password:str
-    profile_picture:Optional[str]=None
-    created_time:datetime=Field(default_factory=datetime.utcnow)
-
-
-class Restaurant(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    address: str
-    contact: str
-    restaurant_pic: Optional[str] = None
-
-class DeliveryPerson(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    mobile: str
-    vehicle: str
+    address:str=Field(max_length=100)
+    created_at:datetime=Field(default_factory=datetime.utcnow)
     profile_picture: Optional[str] = None
+    orders:List["Order"]=Relationship(back_populates="user")
 
-class Order(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+#Restaurant table creation
+class Restaurant(SQLModel,table=True):
+    __tablename__="restaurants"
+    id:Optional[int]=Field(default=None,primary_key=True)
+    name:str=Field(max_length=50)
+    address:str=Field(min_length=5,max_length=100)
+    contact:str
+    created_at:datetime=Field(default_factory=datetime.utcnow)
+    restaurant_pic: Optional[str] = None
+    categories: List["Category"] = Relationship(back_populates="restaurant")
+    menus: List["Menu"] = Relationship(back_populates="restaurant")
+    orders: List["Order"] = Relationship(back_populates="restaurant")
 
-    user_id: int = Field(foreign_key="user.id")
-    restaurant_id: int = Field(foreign_key="restaurant.id")
-    delivery_person_id: int = Field(foreign_key="deliveryperson.id")
-
-    status: str
-    subtotal: float
-    gst_percent: float
-    gst_amount: float
-    delivery_fee: float
-    total_price: float
-
-    placed_time: datetime = Field(default_factory=datetime.utcnow)
-    preparing_time: Optional[datetime] = None
-    pick_up_time: Optional[datetime] = None
-    delivered_time: Optional[datetime] = None
-
-from sqlmodel import SQLModel, Field, Relationship
-from typing import Optional
-
-class OrderItem(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    quantity: int
-    price: float
-
-    order_id: int = Field(foreign_key="order.id")
-    menu_item_id: int = Field(foreign_key="menuitem.id")
-
-
-class MenuItem(SQLModel, table=True):
+# menu table creation
+class Menu(SQLModel, table=True):
+    __tablename__ = "menus"
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     price: float
-    section: str
+    is_available: bool = Field(default=True)
     menu_item_pic: Optional[str] = None
+    restaurant_id: int = Field(foreign_key="restaurants.id")
+    category_id: int = Field(foreign_key="categories.id")
+    category: "Category" = Relationship(back_populates="menus")
+    restaurant: Restaurant = Relationship(back_populates="menus")
+    order_items: List["OrderItem"] = Relationship(back_populates="menu")
 
-    restaurant_id: int = Field(foreign_key="restaurant.id")
+#order table creation
+class Order(SQLModel, table=True):
+    __tablename__ = "orders"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="users.id")
+    restaurant_id: int = Field(foreign_key="restaurants.id")
+    delivery_partner_id: Optional[int] = Field(default=None, foreign_key="delivery_partners.id")
+    total_amount: float
+    status: str = Field(default="PLACED")
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    payment_image: Optional[str] = None
+    user: User = Relationship(back_populates="orders")
+    restaurant: Restaurant = Relationship(back_populates="orders")
+    items: List["OrderItem"] = Relationship(back_populates="order")
+    delivery_partner: Optional["DeliveryPartner"] = Relationship(back_populates="orders")
 
+#orderitem table creation 
+class OrderItem(SQLModel, table=True):
+    __tablename__ = "order_items"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    order_id: int = Field(foreign_key="orders.id")
+    menu_id: int = Field(foreign_key="menus.id")
+    quantity: int
+    price: float  
+    order: Order = Relationship(back_populates="items")
+    menu: Menu = Relationship(back_populates="order_items")
 
+#delivery user table creation
+class DeliveryPartner(SQLModel, table=True):
+    __tablename__ = "delivery_partners"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    mobile: str = Field(unique=True, index=True)
+    vehicle: str  
+    is_available: bool = Field(default=True)
+    delivery_person_profile: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.utcnow)
 
+    orders: List["Order"] = Relationship(back_populates="delivery_partner")
+from datetime import time
+#class category creation 
+class Category(SQLModel, table=True):
+    __tablename__ = "categories"
 
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    start_time: time
+    end_time: time
 
+    restaurant_id: int = Field(foreign_key="restaurants.id")
+    restaurant: "Restaurant" = Relationship(back_populates="categories")
+
+    menus: List["Menu"] = Relationship(back_populates="category")
 
 
 
