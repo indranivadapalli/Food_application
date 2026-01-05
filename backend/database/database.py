892f4b150848306337470cacb55686418b95ff1d
shanmukhapriya26 <shanmukhapriya0526@gmail.com>
2026-01-05 12:33:56 +0530

    delivery updated

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,6 +1,7 @@
+
 from sqlmodel import SQLModel, create_engine, Session
 DATABASE_URL = "sqlite:///./food_delivery_new.db"
-engine = create_engine(DATABASE_URL,echo=True,connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL,pool_size=10,max_overflow=20)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 def get_session():
